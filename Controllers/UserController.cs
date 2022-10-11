5ee7d4b12e27bd195473335825913b459d71cf4e
Karpenya <karpenyalexa1993@gmail.com>
2022-10-12 01:02:34 +0300

    first commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,59 @@
+﻿using HomeWorkMVC1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace HomeWorkMVC1.Controllers
+{
+    public class UserController : Controller
+    {
+
+
+        [HttpGet]
+        public IActionResult InfoModel()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string ProceedRezult(string name, int age, string sex, string button)
+        {
+            UserInfo userInfo = new UserInfo()
+            {
+                Name = name,
+                Age = age,
+                Sex = sex
+            };
+
+
+            using (StreamWriter writer = new StreamWriter(@"D:\user.json", true))
+            {
+                string s = JsonConvert.SerializeObject(userInfo);
+                writer.WriteLine(s);
+
+            }
+
+
+                return ("Thanks bro");
+
+        }
+
+
+
+        public IActionResult UserBase(string button)
+        {
+            List<UserInfo> users = new();
+            string[] lines = System.IO.File.ReadAllLines(@"D:\user.json");
+            foreach (string s in lines)
+            {
+                users.Add(JsonConvert.DeserializeObject<UserInfo>(s));
+            }
+            return View(users);
+        }
+
+    }
+}
+
+       
---
+++ Models/UserInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeWorkMVC1.Models
+{
+    public class UserInfo
+    {
+        [Required(ErrorMessage = "Введите ваше ФИО")]
+        [MaxLength(60, ErrorMessage = "Длинна не должна превышать 60 символов")]
+        public string Name { get; set; }
+
+
+        [Range(1, 100, ErrorMessage = "Недопустимый возраст ")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "ВВедите ваш пол ")]
+        public string Sex { get; set; }
+
+
+
+        public override string ToString()
+        {
+            return $"UserName: {Name}, Age: {Age},Sex: {Sex}";
+           
+        }
+
+
+
+
+
+    }
+}
